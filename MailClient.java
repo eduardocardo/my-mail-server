23c4643ecf41bdcc18eea89ac3e7b3a8536d7aaf
eduardo <eduardocardomartinez@gmail.com>
2014-11-30 20:02:36 +0100

    Añadido metodo statistics que indica en pantalla el numero de mensajes enviados,recibidos y el % de spam

--- MailClient.java
+++ MailClient.java
@@ -13,14 +13,23 @@ public class MailClient
     private String user;
     //indica el ultimo mensaje recibido
     private MailItem lastEmail;
+    //indica el numero de mensajes enviados
+    private int sendEmails;
+    //indica el numero de mensajes recibidos
+    private int receivedEmails;
+    //indica el numero de mensajes spam recibidos
+    private int receivedSpam;
     /**
      * Constructor for objects of class MailClient
      */
     public MailClient(MailServer server,String user)
     {
-        this.server      = server;
-        this.user        = user;
-        lastEmail = null;
+        this.server    = server;
+        this.user      = user;
+        lastEmail      = null;
+        sendEmails     = 0;
+        receivedEmails = 0;
+        receivedSpam = 0;
     }
 
     /**
@@ -31,22 +40,27 @@ public class MailClient
     {
         // recupera el siguiente mensaje
         lastEmail = server.getNextMailItem(user);
+        if(lastEmail != null)
+        {
+            receivedEmails = receivedEmails + 1;
+        }   
         String tempMessage = lastEmail.getMessage();
         if (tempMessage.contains("viagra") || tempMessage.contains("oferta"))
+        {
+
+            if (tempMessage.contains("proyecto"))
             {
-                if (tempMessage.contains("proyecto"))
-                {
-                    return lastEmail;
-                }
-                else
-                {
-                    return null;
-                }
+                return lastEmail;
             }
+            else
+            {
+                receivedSpam = receivedSpam +1;
+                return null;
+            }
+        }
         else
         {
             return lastEmail;
-            
         }
 
     }
@@ -57,26 +71,31 @@ public class MailClient
      */
     public void printNextMailItem()
     {
-       if (server.howManyMailItems(user) > 0)
+        if (server.howManyMailItems(user) > 0)
         {
             lastEmail = server.getNextMailItem(user);
+            receivedEmails = receivedEmails + 1;
             String tempMessage = lastEmail.getMessage();
             if (tempMessage.contains("viagra") || tempMessage.contains("oferta"))
             {
                 if (tempMessage.contains("proyecto"))
                 {
                     lastEmail.print();
+
                 }
                 else
                 {
                     System.out.println("Has recibido spam");
                     lastEmail = null;
+                    receivedSpam = receivedSpam +1;
+
                 }
             }
-                     
+
             else
             {
                 lastEmail.print();
+
             }
         }
         else
@@ -84,7 +103,6 @@ public class MailClient
             System.out.println("No hay mensajes nuevos");
         }
     }
-      
 
     /**
      * Envia un mensaje a otro usuario de email. Introduce el destinatario
@@ -94,6 +112,7 @@ public class MailClient
     {
         lastEmail = new MailItem(user,to,subject,message);
         server.post(lastEmail);
+        sendEmails = sendEmails +1;
     } 
 
     /**
@@ -120,8 +139,10 @@ public class MailClient
             String newSubject = ("RE: "+ email.getSubject());
             String newMessage = email.getMessage() + "\nEstoy de vacaciones";
             sendMailItem(newTo,newSubject,newMessage);
+            sendEmails = sendEmails +1;
         }
     }  
+
     /**
      * Metodo que imprime por pantalla tantas veces como queramos
      * el ultimo mensaje recibido
@@ -138,7 +159,17 @@ public class MailClient
             System.out.println("No hay ningun mensaje");
         } 
     }
-    
+
+    /**
+     * Metodo que imprime por pantalla el numero de mensajes enviados,el numero de mensajes recibidos
+     * el numero de mensajes spam recibidos,el % de mensajes totales que son spam
+     */
+    public void statistics()
+    {
+        System.out.println("El numero de mensajes enviados es : " + sendEmails);
+        System.out.println("El numero de mensajes recibidos es : " + receivedEmails);
+        System.out.println("de los cuales el " + ((receivedSpam*100)/receivedEmails) + "% es spam" ); 
+
+    }                       
 }   
-      
 
